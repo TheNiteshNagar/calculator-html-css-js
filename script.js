87ea28e7cb2ab2d9e3a839544c427629530358bc
Nitesh Nagar <159615163+xitesh@users.noreply.github.com>
2026-01-23 10:55:59 +0530

    html, css, js all done.

--- script.js
+++ script.js
@@ -20,21 +20,15 @@ document.querySelector('.full-screen-button').addEventListener('click', ()=>{
 
 // calculator button
 let inputBox = document.querySelector('input')    // input box 
-let firstValue = ''   //to check is first value is operator or dot or any other non numeric value? 
+let firstNumValue = ''   //to check is first value is operator or dot or any other non numeric value? 
 let isOpenParenthesisAvailable = false      // check is there any open parenthesis is avalaible or not
 let isNumberValueAvailable = false
+let result = ''
+let resultPanel = document.querySelector('.result-panel')
 const buttons = document.querySelectorAll('button')
-// Array.from(buttons).forEach(button => {
-//   button.addEventListener('click', ()=>{
-//     console.log(typeof(button))
-//   })
-// })
+let openParenthesis = false
+let closeParenthesis = false
 
-// buttons.forEach(button => {
-//   button.addEventListener('click', ()=>{
-//     alert('chacha')
-//   })
-// })
 
 for (const index in buttons) {
   if (!Object.hasOwn(buttons, index)) continue;
@@ -44,15 +38,20 @@ for (const index in buttons) {
 
     if(button.id === 'clear') {
       inputBox.value = ''
+      resultPanel.textContent = ''
+      isNumberValueAvailable = false
     } 
     
     else if(button.id === 'parenthesis') {
       if(!isOpenParenthesisAvailable) {
         inputBox.value = inputBox.value + '('
         isOpenParenthesisAvailable = true
-      } else if(isNumberValueAvailable) {
+        openParenthesis = true
+      } else if(isNumberValueAvailable && closeParenthesis) {
         inputBox.value = inputBox.value + ')'
         isOpenParenthesisAvailable = false
+        closeParenthesis = false
+        openParenthesis = false
       }
     }
 
@@ -61,11 +60,25 @@ for (const index in buttons) {
     }
 
     else if(button.id === 'equal') {
-      
+      result = eval(inputBox.value)
+      resultPanel.textContent = result
     }
 
+    else if(button.classList[0] === 'operator' && isNumberValueAvailable){
+      inputBox.value = inputBox.value + button.value
+    }
 
+    else if(button.className === 'dot-button' && isNumberValueAvailable){
+      inputBox.value = inputBox.value + button.value
+    }
 
+    else if(button.classList[0] === 'number') {
+      inputBox.value = inputBox.value + button.value
+      isNumberValueAvailable = true
+      if(openParenthesis){
+        closeParenthesis = true
+      }
+    }
 
   })
 }
